a43c7bcb07b9c2b6968e7aadc961ff6c7d1cd59b
Ghada Farrag <ghain4ghada@gmail.com>
2018-06-29 20:51:11 +0200

    new way to fetch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var staticCacheName = 'cc-static-100';
+var staticCacheName = 'cc-static-200;
 var allCaches = [
     staticCacheName
 ];
@@ -33,44 +33,74 @@ self.addEventListener('activate', function (event) {
 
 });
 
-self.addEventListener('fetch', function (event) {
+//another fetch
+self.addEventListener('fetch', event => {
     var requestUrl = new URL(event.request.url);
-
-        if (requestUrl.origin === location.origin) {
-            if (requestUrl.pathname === '/') {
-                event.respondWith(caches.match('index.html'));
-                return;
-            }
-            if (requestUrl.pathname.endsWith('/')) {
-                //event.respondWith(caches.match('Currency-Converter/index.html'));
-                
-                return fetch('./index.html');
-            }
-            if (requestUrl.pathname.endsWith('index.html')) {
-                event.respondWith(caches.match(requestUrl.pathname.replace('/', '')));
-                return;
-            }
-            if (requestUrl.pathname.endsWith('style.css')) {
-                const index = requestUrl.pathname.indexOf("Currency-Converter/") + 1;
-                event.respondWith(caches.match(requestUrl.pathname.substring(index)));
-                return;
-            }
-            if (requestUrl.pathname.endsWith('.js')) {
-                //event.respondWith(caches.match(requestUrl.pathname.replace('/','')));
-                const index = requestUrl.pathname.indexOf("Currency-Converter/") + 1;
-                event.respondWith(caches.match(requestUrl.pathname.substring(index)));
-                return;
-            
-            }
-        }    
-    
-    console.log(event.request);
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
+    if (requestUrl.origin === location.origin) {
+    event.respondWith(caches.match(event.request)
+        .then(response => response || fetch(event.request)
+            .then(response => caches.open(staticCacheName)
+                .then(cache => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                })).catch(event => {
+                    console.log('Service Worker error caching and fetching');
+                }))
     );
+} else {
+        fetch(event.request).then(response => caches.open(staticCacheName)
+                    .then(cache => {
+                        cache.put(requestUrl.href, response.clone());
+                        return response;
+                    })).catch(event => {
+                        console.log('Service Worker error caching and fetching');
+                    });
+}
+
 });
+
+
+
+
+
+// self.addEventListener('fetch', function (event) {
+//     var requestUrl = new URL(event.request.url);
+
+//         if (requestUrl.origin === location.origin) {
+//             if (requestUrl.pathname === '/') {
+//                 event.respondWith(caches.match('index.html'));
+//                 return;
+//             }
+//             if (requestUrl.pathname.endsWith('/')) {
+//                 //event.respondWith(caches.match('Currency-Converter/index.html'));
+                
+//                 return fetch('./index.html');
+//             }
+//             if (requestUrl.pathname.endsWith('index.html')) {
+//                 event.respondWith(caches.match(requestUrl.pathname.replace('/', '')));
+//                 return;
+//             }
+//             if (requestUrl.pathname.endsWith('style.css')) {
+//                 const index = requestUrl.pathname.indexOf("Currency-Converter/") + 1;
+//                 event.respondWith(caches.match(requestUrl.pathname.substring(index)));
+//                 return;
+//             }
+//             if (requestUrl.pathname.endsWith('.js')) {
+//                 //event.respondWith(caches.match(requestUrl.pathname.replace('/','')));
+//                 const index = requestUrl.pathname.indexOf("Currency-Converter/") + 1;
+//                 event.respondWith(caches.match(requestUrl.pathname.substring(index)));
+//                 return;
+            
+//             }
+//         }    
+    
+//     console.log(event.request);
+//     event.respondWith(
+//         caches.match(event.request).then(function (response) {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
 
 
 self.addEventListener('message', function (event) {
