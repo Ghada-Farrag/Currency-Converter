45e07faf82a8acfb0acc20a5bb0f4505bc42e8e9
Ghada Farrag <ghain4ghada@gmail.com>
2018-06-29 17:40:21 +0200

    test cache updates

--- sw.js
+++ sw.js
@@ -1,21 +1,15 @@
-const staticCacheName = 'cc-static-v50';
+const staticCacheName = 'cc-static-v60';
 
 const allCaches = [
     staticCacheName
 ];
 
 self.addEventListener('install', function (event) {
-    fetch('index.html').then(function (response) {
-        if (!response.ok) {
-            throw new TypeError('Bad response status from index.html');
-        }
-    })
 
     event.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
             return cache.addAll([
                 'index.html',
-                'style.css',
                 'idb.js',
                 'index.js',
                 'IndexController.js'
